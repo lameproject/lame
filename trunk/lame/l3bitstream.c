31456d29ffa9a253a5c02eb9710efe25060a0eeb
markt <markt@981c7233-bbb0-e711-95a9-001517a2e1a4>
1999-11-27 23:39:25 +0000

    More accurate quantization from Segher Boessenkool

--- trunk/lame/l3bitstream.c
+++ trunk/lame/l3bitstream.c
@@ -5,6 +5,10 @@
  * $Id$
  *
  * $Log$
+ * Revision 1.1.1.1  1999/11/24 08:43:09  markt
+ * initial checkin of LAME
+ * Starting with LAME 3.57beta with some modifications
+ *
  * Revision 1.1  1996/02/14 04:04:23  rowlands
  * Initial revision
  *
@@ -554,7 +558,6 @@ static void
 Huffmancodebits( BF_PartHolder **pph, int *ix, gr_info *gi )
 {
     int L3_huffman_coder_count1( BF_PartHolder **pph, struct huffcodetab *h, int v, int w, int x, int y );
-    int bigv_bitcount( int ix[576], gr_info *cod_info );
 
     int region1Start;
     int region2Start;

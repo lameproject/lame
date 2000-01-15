50fb263fcb936789ae2112e34406fe0a5356ce0d
takehiro <takehiro@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-01-15 14:35:38 +0000

    little bit optimization

--- trunk/lame/psymodel.c
+++ trunk/lame/psymodel.c
@@ -5,6 +5,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 1.22  2000/01/13 16:26:50  takehiro
+ * moved info.stereo into gf.stereo
+ *
  * Revision 1.21  2000/01/13 02:07:31  markt
  * moved the rest of the global variables into gf struct
  *
@@ -467,9 +470,9 @@ void L3psycho_anal( short int *buffer[2],
 	b1 = bx_sav[chn][1][j] = bx_sav[chn][0][j];
 	r1 = rx_sav[chn][1][j] = rx_sav[chn][0][j];
 	an = ax_sav[chn][0][j] = wsamp[j];
-	bn = bx_sav[chn][0][j] = j==0 ? wsamp[0] : wsamp[BLKSIZE-j];  // bx[j]; 
+	bn = bx_sav[chn][0][j] = j==0 ? wsamp[0] : wsamp[BLKSIZE-j];  // bx[j];
 	rn = rx_sav[chn][0][j] = sqrt(energy[j]);
-	
+
 	{ /* square (x1,y1) */
 	  if( r1 != 0.0 ) {
 	    numre = (a1*b1);
@@ -710,9 +713,9 @@ void L3psycho_anal( short int *buffer[2],
     
     pe[chn] = 0.0;		/*  calculate percetual entropy */
     for ( b = 0; b < npart_l; b++ ) {
-      FLOAT8 tp = log((thr[b]+1.0) / (eb[b]+1.0) );
-      tp = minimum( 0.0, tp ) ;  /*not log*/
-      pe[chn] -= numlines_l[b] * tp ;
+      if (thr[b] < eb[b]) {
+	pe[chn] -= numlines_l[b] * log((thr[b]+1.0) / (eb[b]+1.0) );
+      }
     }	/* thr[b] -> thr[b]+1.0 : for non sound portition */
     
     

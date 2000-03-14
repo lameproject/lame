e633cd341df7960860f7675e8c042f3d6ea83974
markt <markt@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-03-14 20:45:04 +0000

    removed "info" sturct.   Removing fr_ps struct

--- trunk/lame/l3bitstream.h
+++ trunk/lame/l3bitstream.h
@@ -5,6 +5,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 1.3  2000/02/01 11:26:32  takehiro
+ * scalefactor's structure changed
+ *
  * Revision 1.2  1999/12/03 09:45:30  takehiro
  * little bit cleanup
  *
@@ -24,7 +27,6 @@
 #include "util.h"
 
 void III_format_bitstream( int              bitsPerFrame,
-			   frame_params     *in_fr_ps,
 			   int              l3_enc[2][2][576],
                            III_side_info_t  *l3_side,
 			   III_scalefac_t   scalefac[2][2],

e8e16411ab17bd84d918db30f4a71e5bf8ea7325
markt <markt@981c7233-bbb0-e711-95a9-001517a2e1a4>
2000-04-14 00:37:13 +0000

    tweaks to vbrquantize.c

--- reservoir.h
+++ reservoir.h
@@ -5,6 +5,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 1.4  2000/03/21 23:02:17  markt
+ * replaced all "gf." by gfp->
+ *
  * Revision 1.3  2000/03/14 21:01:47  markt
  * removed fr_ps struct
  *
@@ -33,7 +36,7 @@
 #define RESERVOIR_H
 
 int ResvFrameBegin( lame_global_flags *gfp,III_side_info_t *l3_side, int mean_bits, int frameLength );
-void ResvMaxBits( int mean_bits, int *targ_bits, int *max_bits, int gr);
+void ResvMaxBits( int mean_bits, int *targ_bits, int *max_bits);
 void ResvAdjust(lame_global_flags *gfp,gr_info *gi, III_side_info_t *l3_side, int mean_bits );
 void ResvFrameEnd(lame_global_flags *gfp,III_side_info_t *l3_side, int mean_bits );
 
